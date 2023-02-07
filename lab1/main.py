20dfd63bfe728e4d0c6b0337fc5a09190bcd3948
Fernando engels <fernandoengels@hotmail.com>
2023-02-08 00:12:16 +0100

    lab6

--- lab1/main.py
+++ lab1/main.py
@@ -7,7 +7,10 @@ import json
 import matplotlib.pyplot as plt
 import pandas as pd
 import random
+import copy
 from tabulate import tabulate
+from scipy.special import erfcinv
+
 
 class Signal_information:
     pass
@@ -16,6 +19,8 @@ class Signal_information:
         self.noise_power=0
         self.latency=0
         self.path=path
+        self.copypath=path
+        self.index=0
     def change_signal_power(self,signal_power):
         self.signal_power=signal_power
     def change_noise_power(self,noise_power):
@@ -29,16 +34,35 @@ class Lightpath(Signal_information):
     def __init__(self, signal_power,path,channel):
         super().__init__(signal_power,path)
         self.channel=channel
+        self.R_s=32
+        self.df=50
 class Node:
     pass
-    def __init__(self,node_dict):
+    def __init__(self,node_dict,strategy):
         self.successive={}
         self.label=next(iter(node_dict))
         self.connected_nodes=node_dict[self.label]["connected_nodes"]
         self.position=node_dict[self.label]["position"]
         self.switching_matrix=dict()
+        self.strategy=strategy
+
+
     def propagate(self,signal_information):
+        index=signal_information.index
+        copypath=signal_information.copypath
+        if len(signal_information.path)>=2 and index>=1:
+            self.switching_matrix[copypath[index-1]][copypath[index+1]][signal_information.channel-1]=0
+            try:
+               self.switching_matrix[copypath[index - 1]][copypath[index + 1]][signal_information.channel - 2] = 0
+            except:
+               pass
+            try:
+               self.switching_matrix[copypath[index - 1]][copypath[index + 1]][signal_information.channel] = 0
+            except:
+               pass
+
         signal_information.change_path(signal_information.path[1:])
+        signal_information.index=signal_information.index+1
         if signal_information.path:
             next_node=signal_information.path[0]
             next_line=self.label+next_node
@@ -67,6 +91,10 @@ class Line:
         self.label=label
         self.length=length
         self.state=10*[1]
+        self.n_amplifiers=np.divmod(length,80*10**3)[0]
+        self.gain=10**1.6
+        self.noise_figure=100**0.55
+        self.physical_feat=[0.0460517*1*10**-3,2.13*10**-26*1*10**-24*1*10**-3,1.27*10**-3]
 
     def latency_generation(self,signal_information):
         latency=self.length/((2/3)*3e8)
@@ -93,7 +121,23 @@ class Line:
         signal_information.change_noise_power(signal_information.noise_power + noise)
         next_node=signal_information.path[0]
         self.successive[next_node].probe(signal_information)
-
+    def ase_generation(self):
+        h=6.62607015*10**-34
+        B_n=12.5*10**9#Ghz
+        f=193.414*10**12
+        ase=self.n_amplifiers*(h*f*self.noise_figure*(self.gain-1))
+        return ase
+    def nli_generation(self,power):
+        Nspan=self.n_amplifiers-1
+        B_n=12.5*10**9
+        L_eff=self.length/Nspan
+        R_s=32*10**9
+        df=50*10**9
+        first_part=16/(27*np.pi)*np.log10(np.pi**2/2*self.physical_feat[1]*R_s**2/self.physical_feat[0]*10**(2*R_s/df))
+        sec_part=self.physical_feat[0]/self.physical_feat[1]*self.physical_feat[2]**2*L_eff**2/R_s**3
+        eta=first_part*sec_part
+        Nli=power**3*eta*Nspan*B_n
+        return Nli
 class Network:
     pass
     def __init__(self,name_file):
@@ -101,9 +145,15 @@ class Network:
         self.line={}
         with open(name_file) as json_file:
             data = json.load(json_file)
+
         for i in range(len(data)):
-            node=Node({list(data.keys())[i]:list(data.values())[i]})
+            try:
+                strategy=data[list(data.keys())[i]]["transceiver"]
+            except:
+                strategy="fixed_rate"
+            node=Node({list(data.keys())[i]:list(data.values())[i]},strategy)
             self.node[list(data.keys())[i]]=node
+            self.node[list(data.keys())[i]].switching_matrix=data[list(data.keys())[i]]["switching_matrix"]
             conn_nodes=data[list(data.keys())[i]]["connected_nodes"]
             for j in range(len(conn_nodes)):
                 label=list(data.keys())[i]+conn_nodes[j]
@@ -117,7 +167,7 @@ class Network:
                 if keys != keys2:
                     paths = self.find_all_paths(keys, keys2)
                     for path in paths:
-                        signal_power=1#added 28/12
+                        signal_power=0.001#added 28/12
                         signal_information = Signal_information(signal_power, path)
                         self.probe(signal_information)
                         df1 = pd.DataFrame({'Path': [path], 'Accumulated latency': [signal_information.latency],
@@ -162,10 +212,10 @@ class Network:
                     x=[x * y for x, y in zip(x, y)]
 
             for i in range(len(x)):
-
+                if x[i]==0:
                     self.route_space.loc[
                         self.route_space['Path'].map(tuple) == tuple(path), 'ch ' + str(
-                            i + 1)] = x[i]
+                            i + 1)] = 0
 
 
                #create weithte path with dataframe
@@ -175,13 +225,6 @@ class Network:
            for j in self.node:
                if (i+j) in self.line.keys():
                    self.node[i].successive[i+j]=self.line[i+j]
-                   self.node[i].switching_matrix[j] = dict()
-               for f in self.node:
-                    if (i+j)  in self.line.keys() and (i+f) in self.line.keys():
-                       if j==f:
-                           self.node[i].switching_matrix[j][j] = [0] * len(self.line[list(self.line.keys())[0]].state)
-                       if j!=f:
-                           self.node[i].switching_matrix[j][f] = [1] * len(self.line[list(self.line.keys())[0]].state)
         for p in self.line:
             self.line[p].successive[p[1]]=self.node[p[1]]
 
@@ -303,7 +346,7 @@ class Network:
                 best_path = path
 
         return best_path
-    def stream(self,list_conn,ch,label="latency"):#Define the method stream in the class Network that, for each element
+    def stream(self,list_conn,label="latency"):#Define the method stream in the class Network that, for each element
     #of a given list of instances of the class Connection, sets its latency
     #and snr attribute. These values have to be calculated propagating a
     #SignalInformation instance that has the path that connects the input
@@ -311,13 +354,23 @@ class Network:
     #1path between the considered nodes. The choice of latency or snr has to
     #be made with a label passed as input to the stream function. The label
     #default value has to be set as latency
+        origin_switching_matrices=[0]*len(self.node)
+        for n in range(len(self.node)):
+            origin_switching_matrices[n]=copy.deepcopy(self.node[list(self.node.keys())[n]].switching_matrix)
+
 
         path=[]
         if label=="snr":
             for conn in list_conn:
+                self.update_route_space()
+                ch = random.randrange(1, 11, 1)
+
                 path=self.find_best_snr(self.node[conn.input],self.node[conn.output],ch)
+
                 if path:
+                    conn.bit_rate=self.calculate_bit_rate(path, self.node[path[0]].strategy)
                     signal_information=Lightpath(conn.signal_power,path,ch)
+
                     self.propagate(signal_information)
                     conn.latency=signal_information.latency
                     conn.snr=10*np.log10(signal_information.signal_power/signal_information.noise_power)
@@ -327,8 +380,12 @@ class Network:
 
         if label=="latency":
             for conn in list_conn:
+                self.update_route_space()
+                ch = random.randrange(1, 11, 1)
+
                 path = self.find_best_latency(self.node[conn.input],self.node[conn.output],ch)
                 if path:
+                    conn.bit_rate=self.calculate_bit_rate(path, self.node[path[0]].strategy)
                     signal_information = Signal_information(conn.signal_power, path,ch)
                     self.propagate(signal_information)
                     conn.latency = signal_information.latency
@@ -336,6 +393,36 @@ class Network:
                 if not path:
                     conn.latency = 0
                     conn.snr = "None"
+
+        for n in range(len(self.node)):
+            self.node[list(self.node.keys())[n]].switching_matrix = copy.deepcopy(origin_switching_matrices[n])
+
+    def calculate_bit_rate(self,path,strategy):
+        Bert=1*10**-3
+        r_s=32 #Ghz
+        b_n=12.5#Ghz
+        gsnr=self.weighted_paths.loc[self.route_space['Path'].map(tuple) == tuple(path), 'signal to noise ratio'].values[0]
+        if strategy=="fixed_rate":
+            if gsnr>=2*(erfcinv(2*Bert))**2*r_s/b_n:
+                Rb=100
+            else:
+                Rb=0
+        if strategy=="flex_rate":
+            if gsnr<2*(erfcinv(2*Bert))**2*r_s/b_n:
+                Rb=0
+            if 2*(erfcinv(2*Bert))**2*r_s/b_n <= gsnr < 14/3*(erfcinv(3/2*Bert))**2*r_s/b_n:
+                Rb=100
+            if 14/3*(erfcinv(3/2*Bert))**2*r_s/b_n <= gsnr < 10*(erfcinv(8/3*Bert))**2*r_s/b_n:
+                Rb=200
+            if gsnr >= 10*(erfcinv(8/3*Bert))**2*r_s/b_n:
+                Rb=400
+        if strategy=="shannon":
+            Rb=2*r_s*np.log2(1+gsnr*r_s/b_n)
+
+        return Rb
+
+
+
 
 class Connection:
     pass
@@ -346,7 +433,7 @@ class Connection:
         self.signal_power = signal_power
         self.latency = 0
         self.snr = 0
-
+        self.bit_rate="None"
 
         #plt.plot(*zip(*x), marker='o', color='r', ls='')
 
@@ -365,52 +452,85 @@ if __name__ == '__main__':#Create a main that constructs the network defined by 
 #to be performed in turn for latency and snr path choice. Accordingly, plot
 #the distribution of all the latencies orr the snrs.
 
-    pp=Network("Nodes.json")
-    #
-    signal_power=0.001
-    list_latency=[]
-    list_snr=[]
-    list_snr2=[]
 
-    # for i in range(100):
-    #     conn=[]
-    #     if i%2:
-    #         label="snr"
-    #     else:
-    #         label="latency"
-    #     [input,output]=random.sample(list(pp.node),2)
+    # pp = Network("nodes_not_full.json")
+    # # #
+    signal_power = 0.001
+    # list_latency = []
+    # list_snr = []
+    # list_snr2 = []
     #
-    #     conn=Connection(signal_power,input,output)
-    #     pp.stream([conn],label)
-    #     if label=="snr":
-    #         list_snr.append(conn.snr)
-    #     else:
-    #         list_latency.append(conn.latency)
     #
-    # plt.hist((list_latency))
-    # plt.xlabel("latency")
-    # plt.ylabel("Counts")
-    # plt.show()
+    # for i in range(150):
+    #     conn = []
+    #     [input, output] = random.sample(list(pp.node), 2)
+    #     ch=random.randrange(1, 11, 1)
+    #     label="snr"
+    #     conn = Connection(signal_power, input, output)
+    #     pp.stream([conn], ch,label)
     #
-    # plt.figure(2)
-    # plt.hist(list_snr)
+    #     list_snr2.append(conn.snr)
+    # plt.figure(3)
+    # plt.hist(list_snr2)
     # plt.xlabel("Snr")
     # plt.ylabel("Counts")
     # plt.show()
+    # print(list_snr2)
+    #
+    #
+    #
+    list_snr3 = []
+    list_snr4=[]
+    list_snr3_br=[]
+    list_snr4_br=[]
+    pp = Network("nodes_full_fixed_rate.json")
+    pp.line["AB"].ase_generation()
+    # conn = []
+    # for i in range(100):
+    #
+    #     [input, output] = random.sample(list(pp.node), 2)
+    #
+    #     label = "snr"
+    #     conn.append(Connection(signal_power, input, output))
+    #
+    #
+    # pp.stream(conn, label)
+    #
+    #
+    # for i in range(100):
+    #     list_snr3.append(conn[i].snr)
+    #     list_snr3_br.append(conn[i].bit_rate)
+    # plt.figure(3)
+    # plt.hist(list_snr3)
+    # plt.xlabel("Snr")
+    # plt.ylabel("Counts")
+    # plt.show()
+    # print(list_snr3)
+    # print(list_snr3_br)
+    #
+    # pp2=Network("nodes_full_flex_rate.json")
+    #
+    # pp2.stream(conn,label)
+    #
+    #
+    # for i in range(100):
+    #     list_snr4.append(conn[i].snr)
+    #     list_snr4_br.append(conn[i].bit_rate)
+    # plt.figure(3)
+    # plt.hist(list_snr3)
+    # plt.xlabel("Snr")
+    # plt.ylabel("Counts")
+    # plt.show()
+    # print(list_snr4)
+    # print(list_snr4_br)
 
-    for i in range(100):
-        conn = []
-        [input, output] = random.sample(list(pp.node), 2)
-        ch=random.randrange(1, 11, 1)
-        label="snr"
-        conn = Connection(signal_power, input, output)
-        pp.stream([conn], ch,label)
-
-        list_snr2.append(conn.snr)
-    plt.figure(3)
-    plt.hist(list_snr2)
-    plt.xlabel("Snr")
-    plt.ylabel("Counts")
-    plt.show()
-    print(list_snr2)
-
+# pp=Network("Nodes_full.json")
+    # pp.stream(conn, label)
+    # for i in range(100):
+    #     list_snr4.append(conn[i].snr)
+    # plt.figure(3)
+    # plt.hist(list_snr4)
+    # plt.xlabel("Snr")
+    # plt.ylabel("Counts")
+    # plt.show()
+    # print(list_snr4)
